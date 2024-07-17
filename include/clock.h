51e169b67d008b00c163662345f09f6b9f575cd2
Luis González <ljgonzalez@uc.cl>
2024-07-16 22:48:40 -0400

    ✨ feat(`include/clock.h`): Add new header file for clock management.

---
+++ include/clock.h
@@ -0,0 +1,11 @@
+//
+// Created by luis on 15-07-24.
+//
+
+#ifndef ESPCAM_PROJECT_CLOCK_H
+#define ESPCAM_PROJECT_CLOCK_H
+
+#include "clock/ClockManager.h"
+#include "clock/PosixTimezones.h"
+
+#endif //ESPCAM_PROJECT_CLOCK_H
