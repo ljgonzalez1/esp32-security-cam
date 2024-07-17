c59595f37bb8b0593805a64bc4cceddf9123b996
Luis González <ljgonzalez@uc.cl>
2024-07-16 22:51:01 -0400

    ✨ feat(`GlobalConstants.example.h`): Add example config for WiFi credentials.

---
+++ include/GlobalConstants.example.h
@@ -0,0 +1,7 @@
+#ifndef ESPCAM_PROJECT_GLOBALCONSTANTS_H
+#define ESPCAM_PROJECT_GLOBALCONSTANTS_H
+
+const char* WIFI_SSID = "your wifi network";
+const char* WIFI_PASSWD = "your wifi password";
+
+#endif //ESPCAM_PROJECT_GLOBALCONSTANTS_H
