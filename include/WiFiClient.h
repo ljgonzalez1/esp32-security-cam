4f55f9e4b4a75f60a9bf26f64a221d6c02e1822d
Luis González <ljgonzalez@uc.cl>
2024-07-16 22:47:17 -0400

    ✨ feat(`include/WiFiClient.h`): Añadir la nueva cabecera WiFiClient.h al proyecto.

---
+++ include/WiFiClient.h
@@ -0,0 +1,10 @@
+//
+// Created by luis on 15-07-24.
+//
+
+#ifndef ESPCAM_PROJECT_WIFICLIENT_H
+#define ESPCAM_PROJECT_WIFICLIENT_H
+
+#include "WiFiClient/WiFiClientManager.h"
+
+#endif //ESPCAM_PROJECT_WIFICLIENT_H
